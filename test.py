cf8cc1e3e7eee1a2ffef68a12001cbd4fb4c1ce5
ptran1203 <phatth1203@gmail.com>
2023-02-12 14:23:23 +0700

    test here

--- test.py
+++ test.py
@@ -5,3 +5,4 @@ model = mlflow.pytorch.load_model('runs:/021906e767b44b87b915a9cd4a7d02ef/model'
 model2 = torch.load('/Users/macbook/works/mlflow-example/mlruns/0/021906e767b44b87b915a9cd4a7d02ef/artifacts/model/data/model.pth')
 print(model)
 print(model2)
+print("Test here")
